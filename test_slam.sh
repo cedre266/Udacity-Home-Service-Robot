ed287248b5b2eed51af24ed6e790f0628520c17b
Cedric Perney <cedricperney@hotmail.fr>
2021-05-17 15:20:23 +0000

    test slam with custom world

--- test_slam.sh
+++ test_slam.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd $(pwd)/..; catkin_make
-xterm -e "source /home/workspace/catkin_ws/devel/set_up.bash; roslaunch turtlebot_gazebo turtlebot_world.launch" &
+xterm -e "source /home/workspace/catkin_ws/devel/set_up.bash; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:="/home/workspace/catkin_ws/src/my_robot/worlds/my_world.world"" &
 sleep 5
 xterm -e "source /home/workspace/catkin_ws/devel/set_up.bash; roslaunch turtlebot_gazebo gmapping_demo.launch" &
 sleep 5
