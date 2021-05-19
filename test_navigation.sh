92941b6baf4e35bbefb66c7fee078fca3a92eb2b
Cedric Perney <cedricperney@hotmail.fr>
2021-05-19 08:57:50 +0000

    add custom pgm map for navigation

--- test_navigation.sh
+++ test_navigation.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 xterm -e "source $(pwd)/../devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:="$(pwd)/my_robot/worlds/my_world.world"" &
 sleep 5
-xterm -e "source $(pwd)/../devel/setup.bash; roslaunch turtlebot_gazebo amcl_demo.launch" &
+xterm -e "source $(pwd)/../devel/setup.bash; roslaunch turtlebot_gazebo amcl_demo.launch map_file:="$(pwd)/map/my_map.yaml" initial_pose_a:=-1.57" &
 sleep 5
 xterm -e "source $(pwd)/../devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch"
