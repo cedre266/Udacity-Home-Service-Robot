6283d85a598dc90572cc81ee703d67a71b44c3a1
Cedric Perney <cedricperney@hotmail.fr>
2021-05-19 08:03:59 +0000

    Fix paths to be relative

--- test_slam.sh
+++ test_slam.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
-cd $(pwd)/..; catkin_make
-xterm -e "source /home/workspace/catkin_ws/devel/set_up.bash; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:="/home/workspace/catkin_ws/src/my_robot/worlds/my_world.world"" &
+xterm -e "source $(pwd)/../devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:="$(pwd)/my_robot/worlds/my_world.world"" &
 sleep 5
-xterm -e "source /home/workspace/catkin_ws/devel/set_up.bash; roslaunch turtlebot_gazebo gmapping_demo.launch" &
+xterm -e "source $(pwd)/../devel/setup.bash; roslaunch turtlebot_gazebo gmapping_demo.launch" &
 sleep 5
-xterm -e "source /home/workspace/catkin_ws/devel/set_up.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+xterm -e "source $(pwd)/../devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 sleep 5
-xterm -e "source /home/workspace/catkin_ws/devel/set_up.bash; roslaunch turtlebot_teleop keyboard_teleop.launch"
+xterm -e "source $(pwd)/../devel/setup.bash; roslaunch turtlebot_teleop keyboard_teleop.launch"
+
+# $(rospack find add_markers)
+
+#xterm -e "source /home/workspace/catkin_ws/devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:="/home/workspace/catkin_ws/src/my_robot/worlds/my_world.world"" &
+#sleep 5
+#xterm -e "source /home/workspace/catkin_ws/devel/setup.bash; roslaunch turtlebot_gazebo gmapping_demo.launch" &
+#sleep 5
+#xterm -e "source /home/workspace/catkin_ws/devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+#sleep 5
+#xterm -e "source /home/workspace/catkin_ws/devel/setup.bash; roslaunch turtlebot_teleop keyboard_teleop.launch"
