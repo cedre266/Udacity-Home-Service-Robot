3b1e34dfc3cbd1148d33a10c7b3b11ab3db85ffd
Cedric Perney <cedricperney@hotmail.fr>
2021-05-25 06:14:19 +0000

    Update comments

--- test_navigation.sh
+++ test_navigation.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
+# Launch Turtlebot in custom world
 xterm -e "source $(pwd)/../devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:="$(pwd)/my_robot/worlds/my_world.world"" &
 sleep 5
+# Launch AMCL node and use map of custom world
 xterm -e "source $(pwd)/../devel/setup.bash; roslaunch turtlebot_gazebo amcl_demo.launch map_file:="$(pwd)/map/my_map.yaml" initial_pose_a:=-1.57" &
 sleep 5
+# Launch Rviz set-up for navigation
 xterm -e "source $(pwd)/../devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch"
--- test_slam.sh
+++ test_slam.sh
@@ -1,18 +1,12 @@
 #!/bin/sh
+# Launch Turtlebot in custom world
 xterm -e "source $(pwd)/../devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:="$(pwd)/my_robot/worlds/my_world.world"" &
 sleep 5
+# Launch SLAM node
 xterm -e "source $(pwd)/../devel/setup.bash; roslaunch turtlebot_gazebo gmapping_demo.launch" &
 sleep 5
+# Launch Rviz set-up for navigation
 xterm -e "source $(pwd)/../devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 sleep 5
+# Launch Turtlebot teleoperation node
 xterm -e "source $(pwd)/../devel/setup.bash; roslaunch turtlebot_teleop keyboard_teleop.launch"
-
-# $(rospack find add_markers)
-
-#xterm -e "source /home/workspace/catkin_ws/devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:="/home/workspace/catkin_ws/src/my_robot/worlds/my_world.world"" &
-#sleep 5
-#xterm -e "source /home/workspace/catkin_ws/devel/setup.bash; roslaunch turtlebot_gazebo gmapping_demo.launch" &
-#sleep 5
-#xterm -e "source /home/workspace/catkin_ws/devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch" &
-#sleep 5
-#xterm -e "source /home/workspace/catkin_ws/devel/setup.bash; roslaunch turtlebot_teleop keyboard_teleop.launch"
